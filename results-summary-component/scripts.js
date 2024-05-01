a5b5571200d1be9e5a9b9203d76e9632a3733728
ign-is <igna.dev.contact@gmail.com>
2024-05-01 13:11:46 -0300

    Add JavaScript to dynamically populate the content with JSON data

--- results-summary-component/scripts.js
+++ results-summary-component/scripts.js
@@ -1,2 +1,23 @@
-const result = document.querySelector('result-number');
+import data from './data.json' with { type: 'json' };
+
+const resultNum = document.querySelector('.result-number');
 const ul = document.querySelector('ul');
+
+const sum = data.reduce((acc, obj) => acc + obj.score, 0)
+resultNum.innerText = `${Math.floor(sum / data.length)}`;
+
+const template = data.map(item => {
+    let lowerCaseCategory = item.category.toLocaleLowerCase();
+    return `<li class="${lowerCaseCategory}">
+    <div>
+        <img src="${item.icon}" alt="${lowerCaseCategory}">
+        <p>${item.category}</p>
+    </div>
+    <div class="number">
+        <span class="${lowerCaseCategory}-num bold">${item.score}</span>
+        <span>/ 100</span>
+    </div>
+</li>`;
+});
+
+ul.innerHTML = template.join('');
