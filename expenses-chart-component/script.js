d48b0a8c7488899e8cd5f26608ad68176d4e1670
ign-is <igna.dev.contact@gmail.com>
2024-05-11 12:11:52 -0300

    Add responsive design

--- expenses-chart-component/script.js
+++ expenses-chart-component/script.js
@@ -2,37 +2,30 @@ const amounts = document.querySelectorAll('.amount');
 const days = document.querySelectorAll('.day')
 
 function render(data) {
-
-    for (let i = 0; i < data.length; i++) {
-        amounts[i].innerText = `$${data[i].amount}`;
-        days[i].innerText = `${data[i].day}`;
-    }
-
+  for (let i = 0; i < data.length; i++) {
+      amounts[i].innerText = `$${data[i].amount}`;
+      days[i].innerText = `${data[i].day}`;
+  }
 }
 
 async function fetchData() {
-
-    try {
-      const response = await fetch('data.json');
-      const jsonData = await response.json();
-      render(jsonData);
-    } catch (error) {
-      console.error(error);
-    }
-
+  try {
+    const response = await fetch('data.json');
+    const jsonData = await response.json();
+    render(jsonData);
+  } catch (error) {
+    console.error(error);
   }
-
-
-fetchData();
+}
 
 document.querySelectorAll('.bar').forEach(bar => {
+  bar.addEventListener('mouseover', () => {
+    bar.previousElementSibling.style.visibility = 'visible';
+  });
 
-    bar.addEventListener('mouseover', () => {
-      bar.previousElementSibling.style.visibility = 'visible';
-    });
-
-    bar.addEventListener('mouseout', () => {
-        bar.previousElementSibling.style.visibility = 'hidden';
-    });
-
+  bar.addEventListener('mouseout', () => {
+      bar.previousElementSibling.style.visibility = 'hidden';
+  });
 });
+
+fetchData();
