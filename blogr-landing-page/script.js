dee9ddf91c6752bad40decfd54b3d91a7e2bb440
ign-is <igna.dev.contact@gmail.com>
2024-05-20 18:02:32 -0300

    Add mobile menu functionality

--- blogr-landing-page/script.js
+++ blogr-landing-page/script.js
@@ -1,23 +1,52 @@
 const navDrop = document.querySelectorAll('nav .left > ul:first-of-type > li');
 const iconHamburger = document.querySelector('.icon-hamburger');
+const mobileMenu = document.querySelector('.mobile-menu');
+const mobileMenuLi = document.querySelectorAll('.mobile-menu > ul:first-of-type > li');
 
-let boolean = true;
+let booleanDrop = true;
 navDrop.forEach(item => {
     item.addEventListener('click', () => {
-        if(boolean) {
+        if(booleanDrop) {
             item.children[2].style.display = 'block';
             item.children[1].src = "images/icon-arrow-light.svg";
             item.children[1].style.transform = 'rotate(180deg)'
-            boolean = false;
+            booleanDrop = false;
         } else {
             item.children[2].style.display = 'none';
             item.children[1].src = "images/icon-arrow-dark.svg";
             item.children[1].style.transform = 'rotate(360deg)'
-            boolean = true;
+            booleanDrop = true;
         }
     });
 })
 
-// iconHamburger.addEventListener('click', () => {
-    
-// })
+
+let booleanDropMobile = true;
+mobileMenuLi.forEach(item => {
+    item.addEventListener('click', () => {
+        if(booleanDropMobile) {
+            item.children[2].style.display = 'block';
+            item.children[1].style.transform = 'rotate(180deg)'
+            booleanDropMobile = false;
+        } else {
+            item.children[2].style.display = 'none';
+            item.children[1].style.transform = 'rotate(360deg)'
+            booleanDropMobile = true;
+        }
+    });
+})
+
+
+let booleanHamClose = true;
+iconHamburger.addEventListener('click', () => {
+    console.log('click')
+    if (booleanHamClose) {
+        iconHamburger.src = "images/icon-close.svg";
+        mobileMenu.style.top = '150px';
+        booleanHamClose = false;
+    } else {
+        iconHamburger.src = "images/icon-hamburger.svg";
+        mobileMenu.style.top = '-350px';
+        booleanHamClose = true;
+    }
+})
